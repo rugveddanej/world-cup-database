7ad4354fb9018fbe46057b7ab7b10d14aebcf521
Rugved 💻 <rugveddanej.socialmedia@gmail.com>
2025-06-20 21:58:23 +0530

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -7,31 +7,25 @@ else
   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
 fi
 
-# Vaciar tablas y reiniciar secuencias
 $PSQL "TRUNCATE games, teams RESTART IDENTITY;"
 
-# Leer el archivo CSV línea a línea, saltando la cabecera
 tail -n +2 games.csv | while IFS=',' read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do
-  # Insertar ganador si no existe
   WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER';")
   if [[ -z $WINNER_ID ]]
   then
     $PSQL "INSERT INTO teams(name) VALUES('$WINNER');"
   fi
 
-  # Insertar oponente si no existe
   OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT';")
   if [[ -z $OPPONENT_ID ]]
   then
     $PSQL "INSERT INTO teams(name) VALUES('$OPPONENT');"
   fi
 
-  # Obtener los IDs actualizados
   WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER';")
   OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT';")
 
-  # Insertar el juego
   $PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals)
          VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS);"
 done
